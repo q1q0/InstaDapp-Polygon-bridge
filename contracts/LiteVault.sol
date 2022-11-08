f219f52cb1ecda47916615ff6ecd998b7f54e3be
bergben <bergben@tutanota.com>
2022-11-08 18:27:14 +0200

    feat(LiteVault): add vars visibility

--- contracts/LiteVault.sol
+++ contracts/LiteVault.sol
@@ -23,7 +23,7 @@ contract LiteVault is ERC4626Upgradeable, OwnableUpgradeable {
 
     /// @notice upper limit of percentage values
     /// with 1e6 as base for percentage values 1e8 is 100%
-    uint256 maximumPercentageRange = 1e8;
+    uint256 public constant maximumPercentageRange = 1e8;
 
     /***********************************|
     |           STATE VARIABLES         |
@@ -31,7 +31,7 @@ contract LiteVault is ERC4626Upgradeable, OwnableUpgradeable {
 
     /// @notice list of addresses that are allowed to access toMainnet and fromMainnet functions
     /// modifiable by owner
-    mapping(address => bool) allowedRebalancers;
+    mapping(address => bool) public allowedRebalancers;
 
     /// @notice percentage of token in 1e6 that should remain in the vault when transferring to mainnet.
     /// this number is given in 1e6, i.e. 1% would equal 1_000_000, 10% would be 10_000_000 etc.
@@ -39,28 +39,28 @@ contract LiteVault is ERC4626Upgradeable, OwnableUpgradeable {
     /// then 900k USDC will be transferred to the mainnet iToken vaul
     /// and 100k USDC will sit idle here for instant withdrawals for users.
     /// modifiable by owner
-    uint256 minimumThresholdPercentage;
+    uint256 public minimumThresholdPercentage;
 
     /// @notice address that receives the withdrawal fee
     /// modifiable by owner
-    address withdrawalFeeReceiver;
+    address public withdrawalFeeReceiver;
 
     /// @notice withdrawal fee is either amount in percentage or absolute minimum. This var defines the percentage in 1e6
     /// this number is given in 1e6, i.e. 1% would equal 1_000_000, 10% would be 10_000_000 etc.
     /// modifiable by owner
-    uint256 withdrawalFeePercentage;
+    uint256 public withdrawalFeePercentage;
     /// @notice withdrawal fee is either amount in percentage or absolute minimum. This var defines the absolute minimum
     /// this number is given in decimals for the respective asset of the vault.
     /// modifiable by owner
-    uint256 withdrawalFeeAbsoluteMin;
+    uint256 public withdrawalFeeAbsoluteMin;
 
     /// @notice bridge address to which funds will be transferred to when calling toMainnet
     /// modifiable by owner
-    address bridgeAddress;
+    address public bridgeAddress;
 
     /// @notice exchange price in asset.decimals
     /// modifiable by rebalancers
-    uint256 mainnetExchangePrice;
+    uint256 public mainnetExchangePrice;
 
     /// @notice total (original) raw amount of assets currently committed to invest via bridge
     /// updated in fromMainnet and toMainnet
